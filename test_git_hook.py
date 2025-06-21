9c23f8917b58ade6cd7cade064c805761063d319
Marko Manninen <elonmedia@gmail.com>
2025-06-21 13:47:34 +0300

    Add test file for git post-commit hook testing

---
+++ test_git_hook.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Simple test file to test git post-commit hook functionality
+"""
+
+def simple_function(x):
+    """A simple function for testing."""
+    return x * 2
+
+class TestClass:
+    """A test class."""
+    
+    def __init__(self, value=0):
+        self.value = value
+    
+    def get_value(self):
+        return self.value
