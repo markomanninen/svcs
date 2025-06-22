fc6a6cc22b6f26856b5e014c2f2968afd939c8f0
Marko Manninen <elonmedia@gmail.com>
2025-06-22 23:35:07 +0300

    Enhance PHP test file with interfaces, type hints, and new methods

--- test_enhanced_php.php
+++ test_enhanced_php.php
@@ -7,13 +7,16 @@ use Exception;
 
 /**
  * Demo class for enhanced PHP analysis
+ * Now with additional features
  */
-class DemoClass {
+class DemoClass implements DemoInterface {
     public string $name;
     private $data = [];
+    protected static int $instanceCount = 0;
     
     public function __construct(string $name) {
         $this->name = $name;
+        self::$instanceCount++;
     }
     
     public function getData(): array {
@@ -23,10 +26,24 @@ class DemoClass {
     public function setData(array $data): void {
         $this->data = $data;
     }
+    
+    public function process(): bool {
+        return !empty($this->data);
+    }
+    
+    public static function getInstanceCount(): int {
+        return self::$instanceCount;
+    }
 }
 
-function simpleFunction($param) {
+function simpleFunction(string $param): string {
     return "Hello " . $param;
+}
+
+function newAdvancedFunction(array $items, ?string $prefix = null): array {
+    return array_map(function($item) use ($prefix) {
+        return ($prefix ?? '') . $item;
+    }, $items);
 }
 
 interface DemoInterface {
